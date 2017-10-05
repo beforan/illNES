3cea5a17a78f4941ca956097df7fd201e40b486d
Jonathan Couldridge <jcouldridge@outlook.com>
2017-10-05 21:53:04 +0100

    #16 public interface for the cpu

---
+++ src/illNES.CPU/I6502.cs
@@ -0,0 +1,18 @@
+﻿namespace illNES.CPU
+{
+    public interface I6502
+    {
+        /// <summary>
+        /// Perform a tick of the emulated CPU.
+        /// In theory this would be one 6502 cycle,
+        /// but that may depend on implementation.
+        /// </summary>
+        void Tick();
+
+        /// <summary>
+        /// Dump the current values in all the CPU registers.
+        /// </summary>
+        /// <returns></returns>
+        string DumpRegisters();
+    }
+}
