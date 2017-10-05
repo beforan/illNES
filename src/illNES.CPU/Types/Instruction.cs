35d3454749536ff61484c328d8957bcc91fde86f
Jonathan Couldridge <jcouldridge@outlook.com>
2017-10-05 22:47:30 +0100

    #19 added `Instruction` and supporting types

---
+++ src/illNES.CPU/Types/Instruction.cs
@@ -0,0 +1,41 @@
+﻿namespace illNES.CPU.Types
+{
+    /// <summary>
+    /// Data structure for a single CPU instruction
+    /// </summary>
+    internal class Instruction
+    {
+        /// <summary>
+        /// The instruction's OpCode value
+        /// </summary>
+        public byte OpCode { get; set; }
+
+        /// <summary>
+        /// The friendly Mnemonic for this instruction.
+        /// This is not unique as some Mnemonics are
+        /// multiple ops with different addressing modes.
+        /// </summary>
+        public string Mnemonic { get; set; }
+
+        /// <summary>
+        /// Enum value for the OpCode, purely for nicer code
+        /// </summary>
+        public Ops ExecCode { get; set; }
+
+        /// <summary>
+        /// The addressing mode of this instruction
+        /// </summary>
+        public AddressModes Mode { get; set; }
+
+        /// <summary>
+        /// The byte length of this instruction in memory
+        /// </summary>
+        public int Length { get; set; }
+
+        /// <summary>
+        /// The default number of CPU cycles this instruction expends.
+        /// Extra cycles may be conditionally expended.
+        /// </summary>
+        public int Cycles{ get; set; }
+    }
+}
---
+++ src/illNES.CPU/Types/Ops.cs
@@ -0,0 +1,17 @@
+﻿namespace illNES.CPU.Types
+{
+    /// <summary>
+    /// this is reaaaally just to make life slightly nicer
+    /// for us in the current exec() implementation
+    /// </summary>
+    internal enum Ops
+    { 
+        ADC, AND, ASL, BCC, BCS, BEQ, BIT, BMI,
+        BNE, BPL, BRK, BVC, BVS, CLC, CLD, CLI,
+        CLV, CMP, CPX, CPY, DEC, DEX, DEY, EOR,
+        INC, INX, INY, JMP, JSR, LDA, LDX, LDY,
+        LSR, NOP, ORA, PHA, PHP, PLA, PLP, ROL,
+        ROR, RTI, RTS, SBC, SEC, SED, SEI, STA,
+        STX, STY, TAX, TAY, TSX, TXA, TXS, TYA
+    }
+}
