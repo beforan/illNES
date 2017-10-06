cbaefb5fa058ff89852af483c61856db15bbe0d5
Jonathan Couldridge <jcouldridge@outlook.com>
2017-10-06 08:33:04 +0100

    added PFlags enum, and added registers to IMos6502

---
+++ src/illNES.CPU/IMos6502.cs
@@ -0,0 +1,50 @@
+﻿using illNES.CPU.Types;
+
+namespace illNES.CPU
+{
+    public interface IMos6502
+    {
+        /// <summary>
+        /// Accumulator register
+        /// </summary>
+        byte A { get; }
+
+        /// <summary>
+        /// General purpose X register
+        /// </summary>
+        byte X { get; }
+
+        /// <summary>
+        /// General purpose Y register
+        /// </summary>
+        byte Y { get; }
+
+        /// <summary>
+        /// Stack pointer
+        /// </summary>
+        byte S { get; }
+
+        /// <summary>
+        /// Program Counter
+        /// </summary>
+        ushort PC { get; }
+
+        /// <summary>
+        /// CPU State Flags
+        /// </summary>
+        PFlags P { get; }
+
+        /// <summary>
+        /// Perform a tick of the emulated CPU.
+        /// In theory this would be one 6502 cycle,
+        /// but that may depend on implementation.
+        /// </summary>
+        void Tick();
+
+        /// <summary>
+        /// Dump the current values in all the CPU registers.
+        /// </summary>
+        /// <returns></returns>
+        string DumpRegisters();
+    }
+}
---
+++ src/illNES.CPU/Types/PFlags.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace illNES.CPU.Types
+{
+    [Flags]
+    public enum PFlags
+    {
+        /// <summary>
+        /// A handy flag for no flags set ;)
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// Carry
+        /// </summary>
+        C = 1,
+        /// <summary>
+        /// Zero
+        /// </summary>
+        Z = 2,
+        /// <summary>
+        /// Interrupt
+        /// </summary>
+        I = 4,
+        /// <summary>
+        /// Decimal Math mode
+        /// </summary>
+        D = 8,
+        /// <summary>
+        /// Always Zero
+        /// </summary>
+        B = 16,
+        /// <summary>
+        /// Reset
+        /// </summary>
+        R = 32,
+        /// <summary>
+        /// oVerflow
+        /// </summary>
+        V = 64,
+        /// <summary>
+        /// Negative / Sign
+        /// </summary>
+        N = 128
+    }
+}
