12d4fd38ad92cf1b083aa9b79fb6cf949052e4d8
Jonathan Couldridge <jcouldridge@outlook.com>
2017-10-06 08:47:10 +0100

    #18 `PFlags` docs improved. Added `Mos6502` implementation with ctor and some private fields.

---
+++ src/illNES.CPU/Mos6502.cs
@@ -0,0 +1,54 @@
+﻿using System.Resources;
+using illNES.CPU.Types;
+
+namespace illNES.CPU
+{
+    public class Mos6502 : IMos6502
+    {
+        private readonly IMemoryInterface _m;
+
+        /// <inheritdoc />
+        public byte A { get; private set; }
+
+        /// <inheritdoc />
+        public byte X { get; private set; }
+
+        /// <inheritdoc />
+        public byte Y { get; private set; }
+
+        /// <inheritdoc />
+        public byte S { get; private set; }
+
+        /// <inheritdoc />
+        public ushort PC { get; private set; } = 0xfffc; // Initialise to the reset vector
+
+        /// <inheritdoc />
+        public PFlags P { get; private set; } = PFlags.R | PFlags.I; //the Always 1 flag is set, and we set Interrupt disable at power on
+
+        //Internal state bits
+        private bool _reset = true; //start in reset, means we can execute bootstrap as a reset ;)
+        private bool _nmi;
+        private bool _irq;
+        private bool _xPage;
+        private int _skipCycles;
+
+        public Mos6502(IMemoryInterface m)
+        {
+            _m = m;
+
+            //TODO In the C++ ctor we build an instruction set lookup table here...
+        }
+
+        /// <inheritdoc />
+        public void Tick()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public string DumpRegisters()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- src/illNES.CPU/Types/PFlags.cs
+++ src/illNES.CPU/Types/PFlags.cs
@@ -9,36 +9,64 @@ namespace illNES.CPU.Types
         /// A handy flag for no flags set ;)
         /// </summary>
         None = 0,
+
         /// <summary>
-        /// Carry
+        /// Carry.
+        /// This holds the carry out of the most significant
+        /// bit in any arithmetic operation. In subtraction operations however, this
+        /// flag is cleared - set to 0 - if a borrow is required, set to 1 - if no
+        /// borrow is required. The carry flag is also used in shift and rotate
+        /// logical operations.
         /// </summary>
         C = 1,
+
         /// <summary>
-        /// Zero
+        /// Zero.
+        /// This is set to 1 when any arithmetic or logical
+        /// operation produces a zero result, and is set to 0 if the result is
+        /// non-zero.
         /// </summary>
         Z = 2,
+
         /// <summary>
-        /// Interrupt
+        /// Interrupt Disable.
+        /// This is an interrupt enable/disable flag. If it is set,
+        /// interrupts are disabled. If it is cleared, interrupts are enabled.
         /// </summary>
         I = 4,
+
         /// <summary>
-        /// Decimal Math mode
+        /// Decimal Mode Status.
+        /// This is the decimal mode status flag. When set, and an Add with
+        /// Carry or Subtract with Carry instruction is executed, the source values are
+        /// treated as valid BCD (Binary Coded Decimal, eg. 0x00-0x99 = 0-99) numbers.
+        /// The result generated is also a BCD number.
         /// </summary>
         D = 8,
+
         /// <summary>
-        /// Always Zero
+        /// Break.
+        /// This is set when a software interrupt (BRK instruction) is executed.
         /// </summary>
         B = 16,
+
         /// <summary>
-        /// Reset
+        /// Always Set.
+        /// Not used. Supposed to be logical 1 at all times.
         /// </summary>
         R = 32,
+
         /// <summary>
-        /// oVerflow
+        /// oVerflow.
+        /// When an arithmetic operation produces a result
+        /// too large to be represented in a byte, V is set.
         /// </summary>
         V = 64,
+
         /// <summary>
-        /// Negative / Sign
+        /// Negative / Sign.
+        /// This is set if the result of an operation is
+        /// negative, cleared if positive.
         /// </summary>
         N = 128
     }
