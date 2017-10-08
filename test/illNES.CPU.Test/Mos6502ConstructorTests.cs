08eab9c4145fd3720e7f5d24d1aed8c8fb066d77
Jonathan Couldridge <jcouldridge@outlook.com>
2017-10-08 09:44:45 +0100

    constructior initialisation tested

--- test/illNES.CPU.Test/Mos6502ConstructorTests.cs
+++ test/illNES.CPU.Test/Mos6502ConstructorTests.cs
@@ -1,7 +1,27 @@
-﻿namespace illNES.CPU.Test
+﻿using illNES.CPU.Types;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace illNES.CPU.Test
 {
     public class Mos6502ConstructorTests
     {
+        [Fact]
+        public void InitialValues()
+        {
+            var m = new Mock<IMemoryInterface>();
 
+            var cpu = new Mos6502(m.Object);
+
+            //Most of the registers should be 0
+            Assert.All(
+                new List<byte> { cpu.A, cpu.X, cpu.Y, cpu.S },
+                item => Assert.Equal(0, item));
+
+            Assert.Equal(0xfffc, cpu.PC);
+
+            Assert.Equal(PFlags.R | PFlags.I, cpu.P);
+        }
     }
 }
